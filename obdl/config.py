3b0374389b16ff8c25d241c849b7007470e138e7
Thibault <tmarion90@gmail.com>
2018-12-11 17:25:34 +0100

    Updated watchdog for new rpi_power_board.

--- obdl/config.py
+++ obdl/config.py
@@ -1,8 +1,9 @@
 
-root_folder = '/opt/ctahr/'
+root_folder = '/opt/obdl/'
 port = 51641
 
 ## PIN CONFIGURATION ##
 # System / Misc
-watchdog_pin = 20
-powerstate_pin = 23
+watchdog_pin = 22
+powerstate_pin = 26
+activity_pin = 16
--- watchdog.py
+++ watchdog.py
@@ -2,7 +2,14 @@
 
 import RPi.GPIO as GPIO
 import obdl.config as config
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(config.watchdog_pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(config.watchdog_pin, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(config.activity_pin, GPIO.OUT, initial=GPIO.HIGH)
+
+while True:
+    GPIO.output(config.watchdog_pin, not GPIO.input(config.watchdog_pin))
+    GPIO.output(config.activity_pin, not GPIO.input(config.activity_pin))
+    time.sleep(.1)
