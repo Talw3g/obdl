6fab4b770cbe4ff18486442bf0681debce15368e
Thibault <tmarion90@gmail.com>
2018-12-11 17:41:17 +0100

    Added powerstate_guard

---
+++ powerstate_guard.py
@@ -0,0 +1,15 @@
+#! /home/thibault/.virtualenvs/obd/bin/python3
+
+import RPi.GPIO as GPIO
+import obdl.config as config
+import os,time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(config.powerstate_pin, GPIO.IN)
+
+while True:
+    time.sleep(0.1)
+    if not GPIO.input(config.powerstate_pin):
+        os.system("shutdown -h now")
+
